fe6450463bf8d90762e0293f0d22894f9e68dce0
Dileep R <dileep.r@utthunga.in>
2019-03-25 17:59:08 +0530

    Updates

---
+++ precache-manifest.02c010dc5b21a2b372b2a0c45eb45cfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/portfolio/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "11569303dbb846762ff1",
+    "url": "/portfolio/static/js/main.ff1405c7.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/portfolio/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "11569303dbb846762ff1",
+    "url": "/portfolio/static/css/main.f3a456d2.chunk.css"
+  },
+  {
+    "revision": "25dfbe26f16807b01ccd7c7c02627dab",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/rajadileepkumar/precache-manifest.2e9f3fb3ce0c8ca63fc931e1a5f78af1.js"
+  "/portfolio/precache-manifest.02c010dc5b21a2b372b2a0c45eb45cfc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/rajadileepkumar/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
